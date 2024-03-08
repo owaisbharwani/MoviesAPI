4c4624ae76645d43fdfff48243d2a8669f6d7bd4
owaisbharwani <owaisbharwani96@hotmail.com>
2024-03-08 07:34:08 +0000

    final back end commit

---
+++ src/main/java/dev/owais/movies/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.owais.movies.repositories;
+
+import dev.owais.movies.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
